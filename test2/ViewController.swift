b09de57597132be3472c74875bcb750640150811
kranthi <kranthi@kranthis-MacBook-Pro.local>
2017-08-19 11:05:56 -0700

    initial commit

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -10,10 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func shareonimage(_ sender: Any) {
+        var  urlWhats = "whatsapp://app"
+        if let urlString = urlWhats.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet()) {
+            if let whatsappURL = NSURL(string: urlString) {
+                
+                if UIApplication.sharedApplication().canOpenURL(whatsappURL) {
+                    
+                    if let image = UIImage(named: "image") {
+                        if let imageData = UIImageJPEGRepresentation(image, 1.0) {
+                            let tempFile = NSURL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent("Documents/whatsAppTmp.wai")
+                            do {
+                                try imageData.writeToURL(tempFile, options: .DataWritingAtomic)
+                                self.documentInteractionController = UIDocumentInteractionController(URL: tempFile)
+                                self.documentInteractionController.UTI = "net.whatsapp.image"
+                                self.documentInteractionController.presentOpenInMenuFromRect(CGRectZero, inView: self.view, animated: true)
+                            } catch {
+                                print(error)
+                            }
+                        }
+                    }
+                    
+                } else {
+                    // Cannot open whatsapp
+                }
+            }
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello World Of github")
+        
+        
 
     }
 
